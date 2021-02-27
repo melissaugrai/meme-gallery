83a61027c38cb001c3d389612ac923d7d5436072
Melissa Ugrai <mugrai@gmail.com>
2021-02-27 14:27:05 -0800

    Add last JavaScript to get function of adding images

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
-let imageUrls = [];
+var imageUrls = [];
+
+const gallery = document.querySelector('main');
 const imageUrlInput = document.querySelector('input');
 const addImageButton = document.querySelector('button');
 
-addImageButton.addEventListener('click', function () {
+addImageButton.addEventListener('click', function (event) {
 
     if (imageUrlInput.value !== '') {
         imageUrls.push(imageUrlInput.value);
     }
     imageUrlInput.value = '';
+    updateGallery();
 });
+
+function updateGallery() {
+    gallery.innerHTML = '';
+    for (let i = 0; i < imageUrls.length; i++) {
+        let imageElement = document.createElement('img');
+        imageElement.className = 'gallery-image';
+        imageElement.src = imageUrls[i];
+        gallery.appendChild(imageElement);
+    }
+}
+updateGallery();
